a04eeab496cc04e55dcead0c5352e3ddfb3a6e0a
Allex Machado <itsnotallexmachado@gmail.com>
2025-05-16 16:13:02 -0300

    Update

---
+++ ex008/exercise.js
@@ -0,0 +1,8 @@
+var vel = 
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60){ // Essa condição é classificada como 'Condição Simples', pois não há mais condicões extras!
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
